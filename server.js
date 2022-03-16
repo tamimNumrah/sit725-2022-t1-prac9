203063f7a41e9b981e7cd94db129ca903cd0a828
tamimNumrah <tamim@numrah.com>
2022-03-16 18:30:33 +1100

    GET api to add two numbers

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(express.urlencoded({ extended: false}));
 //   res.send('Hello World');
 // })
 
+const addNumbers = (number1, number2) => {
+    var num1 = parseInt(number1);
+    var num2 = parseInt(number2);
+    return num1 + num2;
+};
+
+app.get("/addTwoNumbers", (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = addNumbers(number1, number2);
+    res.json({
+        statusCode: 200,
+        data: result,
+        message: "Success"
+    });
+});
+
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
