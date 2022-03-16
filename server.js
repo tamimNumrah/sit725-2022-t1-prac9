ca91d7fa8c32d0a7ebf894079cfb77b720705098
tamimNumrah <tamim@numrah.com>
2022-03-16 18:13:50 +1100

    added simple html file and public folder

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World');
+// })
 
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
-    console.log("App listening to: " + port);
+    console.log("App running at http://localhost: " + port);
 });
